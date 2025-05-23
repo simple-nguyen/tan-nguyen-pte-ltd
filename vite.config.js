2e3a8159decf322ab7be783449ff1af7d10d80f6
simple-nguyen <24709890+simple-nguyen@users.noreply.github.com>
2025-05-24 00:19:58 +0700

    fix: update base path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: '/tan-nguyen-pte-ltd/',  // Set base path for GitHub Pages
+  base: '/',  // Set base path for GitHub Pages
 })
